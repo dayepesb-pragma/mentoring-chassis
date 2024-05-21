f1f762982f5c5d42b8efc8a9845957d96e38a9a9
David Yepes <david.yepesb@pragma.com.co>
2024-05-21 01:42:49 -0500

    Add alert Service custom promps

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
-import { registerApplication, start } from 'single-spa';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { registerApplication, start, unregisterApplication } from 'single-spa';
+import { AlertService } from '../../service/alert.service';
 import { LoadingService } from '../../service/loading.service';
 
 @Component({
@@ -9,22 +10,28 @@ import { LoadingService } from '../../service/loading.service';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnDestroy, OnInit {
 
   private _loadingService: LoadingService = inject(LoadingService);
+  private _alertService: AlertService = inject(AlertService);
 
-  constructor() {
+  ngOnInit() {
     registerApplication({
       name: 'mentoring-app',
       app: () => System.import('mentoring-app').then((module) => module.default),
       activeWhen: location => location.pathname.startsWith('/dashboard'),
       customProps: {
         loadingService: this._loadingService,
+        alertService: this._alertService
       }
     });
 
     start({
       urlRerouteOnly: true,
     });
+  }
+
+  ngOnDestroy() {
+    unregisterApplication('mentoring-app');
   }
 }
